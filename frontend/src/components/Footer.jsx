13c4bc35be02550f471c567099664d10be308da6
Mudit Rawat <mr930542@gmail.com>
2026-03-30 17:38:21 +0530

    Initial commit

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,18 +5,18 @@ const Footer = () => {
     <footer>
       <div className="container">
         <div className="banner">
-          <div className="left">ZEESHU</div>
+          <div className="left">Mudit Rawat</div>
           <div className="right">
-            <p>Z6 Gulshan-e-Maymar, Karachi</p>
+            <p>Delhi, India</p>
             <p>Open: 05:00 PM - 12:00 AM</p>
           </div>
         </div>
         <div className="banner">
           <div className="left">
-            <p>Developed By CODEWITHZEESHU</p>
+            <p>Developed By Mudit Rawat</p>
           </div>
           <div className="right">
-            <p>All Rights Reserved By CodeWithZeeshu.</p>
+            <p>All Rights Reserved By Mudit Rawat</p>
           </div>
         </div>
       </div>
